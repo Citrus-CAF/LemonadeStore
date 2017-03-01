d1dda45bf6ee6e684e19d1ae1a13c64e4a19555c
Alex Cruz <du.alexcruz@gmail.com>
2017-03-01 15:26:13 +0000

    LemonadeStore: Import update-themes script from DU-Scripts
    commit ea3af9a255da006727d575aa3ae8f3345a6e7043
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Mon Nov 21 04:43:22 2016 +0100
    
        Clean up folders
    
        - Update script
    
    commit 733e4fff74640e6b24134b2ebab3f06f1776ef63
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Fri Jun 17 02:30:41 2016 -0400
    
        Theme Resources updated!
    
    commit 2246ad9bef47006b46d8c317f1ae9b6ad11c2c5a
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Fri May 13 01:25:29 2016 -0400
    
        Get that DUI!!
    
    commit 2a00c7e017c7ace63aed4a79c0b3df834c6f6251
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Wed May 11 18:19:46 2016 -0400
    
        Update theme-themes script
    
    commit 5f2debcc6bb02b3fc85d699c54a7d0b49956a600
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Thu Mar 24 08:36:43 2016 -0400
    
        Change repo name to DU-Scripts
    
    commit c10829822f774c412201a7cd2f5d404659f8a259
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Tue Feb 16 21:28:04 2016 -0500
    
        Update the script to add back in Android Manifest and attrs
    
    commit 66126d79544552666e4d3b5c1cd06c072399fa89
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Wed Jan 6 05:40:04 2016 +0000
    
        Should be good moving forward now
    
        - Updated theme resource and script
    
    commit 8abbe5bb3bb87ad8c106c425e9ae3c259a9e5641
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Wed Jan 6 05:29:11 2016 +0000
    
        Update script once more!
    
    commit 51106922aa021c5566a6517a5865f3543470c42b
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Wed Jan 6 04:46:00 2016 +0000
    
        Updated the script
    
    commit d3f9cb7e8794570aa5b8ba6806b128469e5daa40
    Author: Jmz <jmztaylor@gmail.com>
    Date:   Wed Jul 29 21:09:42 2015 +0000
    
        Make script easier to read
    
        Change-Id: I1ba7b360625231c3997860e164cb9a30248031ae
    
    commit eee0e80a54b79450424e812aedce6c8c22e4ed6e
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Wed Jul 29 12:23:01 2015 +0000
    
        Update Readme, include script to update theme resources
    
        - Currently the script is very long but it works. As time goes by,
        I'm sure we can consolidate some of it.

---
+++ update-themes.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+# Copyright (C) 2015 DirtyUnicorns
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+WORKING_DIR=~/ctr
+
+function delete_useless () {
+  declare -a array=($@)
+  for i in `seq 0 $(( ${#array[@]} - 1 ))`
+  do
+    rm -rf "${array[i]}"
+  done
+}
+
+function process_all () {
+  declare -a array=($@)
+  for i in `seq 0 $(( ${#array[@]} - 1 ))`
+  do
+    echo "Removing unneeded items in ${array[i]}"
+    if [[ ${array[i]} == frameworks* ]]; then
+      cd $WORKING_DIR/${array[i]}
+      echo $WORKING_DIR/${array[i]}
+    else
+      cd $WORKING_DIR/packages/apps
+    fi
+    if [ -d ${array[i]} ]; then
+      cd ${array[i]}
+      delete_useless ${common[@]}
+      cd res
+      delete_useless ${res[@]}
+      cd values
+      delete_useless ${values[@]}
+    else
+      echo "${array[i]]} package not found"
+    fi
+    cd $WORKING_DIR
+  done
+}
+
+function copy_all () {
+  declare -a array=($@)
+  for i in `seq 0 $(( ${#array[@]} - 1 ))`
+  do
+    cd $WORKING_DIR/packages/apps
+    cp -r ${array[i]} $WORKING_DIR/LemonadeStore/themes-resources/packages/apps/
+  done
+}
+
+declare -a root=('LemonadeStore' 'abi' 'bionic' 'art' 'bootable' 'build' 'dalvik' 'development' 'device' 'external' 'frameworks' 'hardware' 'kernel' 'libcore' 'libnativehelper' 'manifest' 'ndk' 'out' 'packages' 'pdk' 'prebuilts'
+                 'sdk' 'system' 'tools' 'vendor')
+
+declare -a frameworks=('api' 'cmds' 'data' 'docs' 'drm' 'graphics' 'include' 'keystore' 'libs' 'location' 'media' 'native' 'nfc-extras' 'obex' 'opengl' 'policy' 'rs' 'samples' 'sax' 'security-bridge' 'services' 'telecomm'
+                       'telephony' 'test-runner' 'tests' 'tools' 'wifi')
+
+declare -a common=('common.mk' '.classpath' '.git' '.gitignore' '.idea' '.project' 'assets' 'build.gradle' 'src' 'tests' 'Android.mk' 'CleanSpec.mk' 'lint.xml' 'MODULE_LICENSE_APACHE2' 'NOTICE' 'preloaded-classes'
+                   'proguard.cfg' 'proguard.flags' 'proguard-test.flags' 'wrap_alpha.py' 'tools' 'tests' 'extensions_src' 'mkprojectfile' 'project.properties' 'hero.png' 'LICENSE' 'README.md' 'script' 'jni' 'res_p' 'sounds'
+                   'src_pd' 'src_pd_gcam' 'tests_camera' 'version.mk' 'COPYING' 'ReadMe.md' 'WallpaperPicker' 'protos' 'util' 'fill_screens.py' 'print_db.py' 'update_gallery_files.py' 'update_system_wallpaper_cropper.py'
+                   'cyanoden-dsp' 'emailcommon' 'transition' 'TestCommon' 'provider_src' 'gallerycommon' 'jni_jpegstream' 'apptests' 'libs' 'info_lookup' 'src_plugin' 'perf-tests' 'app-perf-tests' 'src-N' 'src-pre-N' 'build-app.gradle' 'build-library.gradle' 'local.properties' 'gradlew.bat' 'gradlew' 'gradle' 'build.gradle' '.idea' 'settings.gradle' 'gradle.properties' 'proguard-rules.pro' 'v' '*.jar' 'build' 'ForceProguard.mk' 'proguard-release.flags' 'Readme.md')
+
+declare -a packages=('StatementService' 'BackupRestoreConfirmation' 'CaptivePortalLogin' 'DefaultContainerService' 'ExternalStorageProvider' 'FakeOemFeatures' 'FusedLocation' 'InputDevices' 'Keyguard' 'PrintSpooler' 'SettingsProvider'
+                     'SharedStorageBackup' 'Shell' 'VpnDialogs' 'WAPPushManager' 'WAPPushManager' 'WallpaperCropper' 'services' 'WallpaperBackup' 'PrintRecommendationService' 'Osu' 'MtpDocumentsProvider' 'ExtShared' 'ExtServices' 'CtsShim')
+
+declare -a res=('Android.mk' 'AndroidManifest.xml' 'MODULE_LICENSE_APACHE2' 'NOTICE' 'lint.xml' 'assets' 'menu*' 'values-*' 'xml*' 'interpolator' 'raw*' 'anim*' 'animator' 'transition' 'MakeJavaSymbols.sed' 'tests' 'src' 'build.gradle' 'proguard.flags' 'setting' 'gradlew' 'gradlew.bat')
+
+declare -a values=('all_search_engines.xml' 'appmsg_colors.xml' 'arrays.xml' 'attrs_manifest.xml' 'bookmarks_icons.xml' 'bools.xml' 'config.xml' 'defaults.xml' 'donottranslate.xml' 'donottranslate_config.xml'
+                   'aliases.xml' 'crop_colors.xml' 'dslv_attrs.xml' 'animation_constants.xml' 'donottranslate-cldr.xml' 'donottranslate-maps.xml' 'donottranslate-names.xml' 'donottranslate_material.xml'
+                   'donottranslate-search_engines.xml' 'custom_arrays.xml' 'custom_strings.xml' 'custom_symbols.xml' 'custom_dimens.xml' 'custom_attrs.xml' 'custom_colors.xml' 'fractions.xml' 'ids.xml' 'integers.xml' 'internal.xml' 'keys.xml' 'lland_config.xml' 'lland_strings.xml' 'plurals.xml'
+                   'public.xml' 'strings.xml' 'symbols.xml' 'vpi_attrs.xml' 'vpi_defaults.xml' 'cm_arrays.xml' 'cm_plurals.xml' 'cm_strings.xml' 'custom_strings.xml' 'custom_arrays.xml' 'vpi__defaults.xml' 'vpi__attrs.xml'
+                   'nontranslatables.xml' 'accountprovider.xml' 'constants.xml' 'codeaurora_strings.xml' 'filtershow_ids.xml' 'filtershow_strings.xml' 'filtershow_values.xml' 'filtershow_values_attrs.xml' 'projekt_colors.xml' 'projekt_config.xml' 'projekt_symbols.xml' 'projekt_styles.xml' 'projekt_symbols.xml' 'projekt_colors.xml' 'projekt_strings.xml')
+
+declare -a core=('java' 'jni' 'tests' 'src')
+
+declare -a theme_packages=('Calculator' 'Calendar' 'CalendarWidget' 'Camera2' 'Contacts' 'ContactsCommon' 'DUI' 'Custom-Squash' 'DeskClock' 'Dialer' 'Gallery2'
+                           'Launcher3' 'Messaging' 'OmniSwitch' 'PhoneCommon' 'Settings' 'Stk')
+
+declare -a extra=('java' '.idea' 'gradle' '.gitignore' 'build.gradle' 'gradlew' 'gradlew.bat' 'local.properties' 'proguard-rules.pro' 'proguard.flags')
+
+cd $WORKING_DIR
+echo "Removing files so we can clean sync"
+delete_useless ${root[@]}
+echo "Repo Syncing........."
+repo sync -c --no-tags >> /dev/null
+if [ $? -eq 0 ]; then
+  echo "Repo Sync success"
+else
+  echo "Repo Sync failure"
+  exit 1
+fi
+echo "Cloning Theme Resources repo"
+git clone https://github.com/Citrus-CAF/LemonadeStore.git
+echo "Removing unneeded files"
+cd frameworks/base
+delete_useless ${frameworks[@]}
+cd packages
+delete_useless ${packages[@]}
+cd SystemUI
+delete_useless ${common[@]}
+rm -rf docs
+cd res
+delete_useless ${res[@]}
+cd values
+delete_useless ${values[@]}
+cd ../../../SettingsLib
+delete_useless ${common[@]}
+cd res
+delete_useless ${res[@]}
+cd values
+delete_useless ${values[@]}
+cd ../../../DocumentsUI
+delete_useless ${common[@]}
+cd res
+delete_useless ${res[@]}
+cd values
+delete_useless ${values[@]}
+cd ../../../EasterEgg
+delete_useless ${common[@]}
+cd res
+delete_useless ${res[@]}
+cd values
+delete_useless ${values[@]}
+cd ../../../../core
+delete_useless ${core[@]}
+cd res
+delete_useless ${res[@]}
+cd res
+delete_useless ${res[@]}
+cd values
+delete_useless ${values[@]}
+rm -rf  $WORKING_DIR/LemonadeStore/themes-resources/frameworks
+cd $WORKING_DIR
+process_all ${theme_packages[@]}
+echo "Cleaning target folders"
+rm -rf LemonadeStore/themes-resources/packages
+mkdir LemonadeStore/themes-resources/packages
+mkdir LemonadeStore/themes-resources/packages/apps
+mkdir LemonadeStore/themes-resources/frameworks
+echo "Copying all files to $WORKING_DIR/LemonadeStore/themes-resources"
+copy_all ${theme_packages[@]}
+cp -r $WORKING_DIR/frameworks/base/core $WORKING_DIR/frameworks/base/packages $WORKING_DIR/LemonadeStore/themes-resources/frameworks
+cd ~/ctr/LemonadeStore/themes-resources
+cd $WORKING_DIR/LemonadeStore/themes-resources/packages/apps/Dialer/InCallUI
+delete_useless ${res[@]}
+cd $WORKING_DIR/LemonadeStore/themes-resources
